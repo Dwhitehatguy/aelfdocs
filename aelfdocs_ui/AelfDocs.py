8908c57351d458a1dd1453a9386a72bcc7e84bc6
Dwhitehatguy <123422408+Dwhitehatguy@users.noreply.github.com>
2024-07-30 00:19:14 +0100

    Hot fix

--- aelfdocs_ui/AelfDocs.py
+++ aelfdocs_ui/AelfDocs.py
@@ -1,15 +1,11 @@
 import streamlit as st
 st.title("AelfDocs")
 
-# Load Settings
-
-# Change system path to root direcotry
-import os, sys, pymongo
-
+# Change system path to root directory
+import sys
 sys.path.insert(0, '../')
 
 ATLAS_URI = st.secrets["ATLAS_URI"]
-
 if not ATLAS_URI:
     raise Exception ("'ATLAS_URI' is not set.  Please set it above to continue...")
 else:
@@ -20,31 +16,28 @@ DB_NAME = 'aelfdocs'
 COLLECTION_NAME = 'docs'
 INDEX_NAME = 'idx_embedding'
 
-# LlamaIndex will download embeddings models as needed
-# Set llamaindex cache dir to ../cache dir here (Default is system tmp)
-# This way, we can easily see downloaded artifacts
+# Set where embeddings model will be downloaded
+import os
 os.environ['LLAMA_INDEX_CACHE_DIR'] = os.path.join(os.path.abspath('../'), 'cache')
 
+import pymongo
 mongodb_client = pymongo.MongoClient(ATLAS_URI)
 
-# Setup Embedding Model
+# Setup Embedding Model and LLM
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
-from llama_index.core import VectorStoreIndex
 from llama_index.llms.gemini import Gemini
 from llama_index.core import Settings
-from llama_index.core import StorageContext
-from llama_index.core.memory import ChatMemoryBuffer
 
 embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.embed_model = embed_model
-
-# Setup LLM
-
 llm = Gemini(api_key=st.secrets["GOOGLE_API_KEY"], model="models/gemini-1.5-flash-latest")
 Settings.llm = llm
 
 # Connect Llama-index and MongoDB Atlas
+from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
+from llama_index.core import StorageContext
+from llama_index.core import VectorStoreIndex
+
 vector_store = MongoDBAtlasVectorSearch(mongodb_client = mongodb_client,
                                  db_name = DB_NAME, collection_name = COLLECTION_NAME,
                                  index_name  = 'idx_embedding',
@@ -54,6 +47,8 @@ index = VectorStoreIndex.from_vector_store(
     vector_store=vector_store,
 )
 
+# Initialize memory for chat engine
+from llama_index.core.memory import ChatMemoryBuffer
 memory = ChatMemoryBuffer.from_defaults()
 
 chat_engine = index.as_chat_engine(
